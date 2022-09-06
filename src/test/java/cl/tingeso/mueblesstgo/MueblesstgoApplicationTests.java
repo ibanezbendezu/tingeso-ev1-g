cc529ec3ec91f62af841b0f6e01e60d44d84e9cc
main <aaron.ibanez@usach.cl>
2022-09-06 16:19:57 -0300

    Proyecto creado

---
+++ src/test/java/cl/tingeso/mueblesstgo/MueblesstgoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tingeso.mueblesstgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MueblesstgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
